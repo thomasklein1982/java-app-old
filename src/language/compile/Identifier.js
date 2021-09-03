be25144147a12325dc30b2cf329793cf13b7495e
Thomas Klein <thomas.klein.1982@gmail.com>
2021-09-03 23:36:38 +0200

    identifier, Fieldaccess parsen (mit Context)

--- src/language/compile/Identifier.js
+++ src/language/compile/Identifier.js
@@ -1,9 +1,26 @@
-export function Identifier(node,source,scope,errors){
+import { Java } from "../java";
+
+export function Identifier(node,source,scope,errors,context){
   console.log(node);
   let name=source.getText(node);
-  let obj=scope.get(name);
+  let obj;
+  if(context && context.ownerClazz){
+    obj=context.ownerClazz.getAttribute(context.static);
+    if(!obj){
+      errors.push(source.createError("Die Klasse "+context.ownerClazz.name+" hat kein "+(context.static? "statisches":"")+" Attribut ",node));
+    }
+  }else{
+    obj=scope.getLocalVariable(name);
+    if(!obj){
+      obj=scope.getClazzByName(name);
+    }
+    if(!obj){
+      errors.push(source.createError("'"+name+"' ist undefiniert",node));
+    }
+  }
+  
   return {
-    code: source.getText(node),
-    type: Java.datatypes.String
+    code: name,
+    object: obj
   };
 }
