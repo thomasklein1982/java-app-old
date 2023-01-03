c5f174b3b4a38a3f3ec88f34e177a83e4a46b5ec
Thomas Klein <thomas.klein.1982@gmail.com>
2023-01-03 01:49:50 +0100

    primeicons sw globPatterns

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,10 @@ export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
-      assetsInclude: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png', 'icon-transparent.png','Logo.png', 'additionalJSCode.js','icon-transparent.png','assets/primeicons.c9eaf535.eot','assets/*.ttf'],  
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,ttf,eot,woff,woff2}']
+      },
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png', 'icon-transparent.png','Logo.png', 'additionalJSCode.js','icon-transparent.png','assets/primeicons.c9eaf535.eot','assets/*.ttf'],  
       //assetsInclude: ["assets/*.ttf"],
       manifest: {
         name: 'JavaApp',
